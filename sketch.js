0b01be226f5f942e1a069337d73e36bb8d8114a3
David Tudury <david.tudury@gmail.com>
2019-05-03 00:03:07 -0700

    linear interpolate

--- sketch.js
+++ sketch.js
@@ -12,35 +12,28 @@ class Values {
     this.total += value
   }
   split () {
-    let average = this.total / this.count
+    let subtotal = this.total - this.low - this.high
     let middle = (this.low + this.high) / 2
-    let delta = average - middle
-    // 4 -> 3.5     | Math.sqrt(Math.pow(14  , 2) / 16) | 0.75
-    // 6 -> 2.75    | Math.sqrt(Math.pow(11  , 2) / 16) | 0.25 * 3
-    // 8 -> 2.5     | Math.sqrt(Math.pow(10  , 2) / 16) | 0.125 * 2
-    // 10 -> 2.375  | Math.sqrt(Math.pow( 9.5, 2) / 16) | 0.075 * 1 2/3
-    // 12 -> 2.3    | Math.sqrt(Math.pow( 9.2, 2) / 16) | 0.05 * 1 1/2
-    // 14 -> 2.25   | Math.sqrt(Math.pow( 9  , 2) / 16)
-    let weight = average + delta * 3.5
-    console.log(this.count)
-    console.log((this.low + this.high) / 2)
-    console.log(average)
-    console.log(weight)
-    console.log(delta)
-    console.log('-----')
-    let outValue = new Values()
-    for (let i = 0; i < this.count; i++) {
-      let t = i / (this.count - 1)
-      let it = 1 - t
-      let value = this.low * it * it + weight * 2 * it * t + this.high * t * t
-      console.log(value)
-      outValue.addValue(value)
+    let average = subtotal / (this.count - 2)
+    let values = new Values()
+    values.addValue(this.low)
+    values.addValue(this.high)
+    if (middle > average) {
+      let step = ((average - this.low) * 2 / (this.count - 1))
+      for (let i = 1; i < this.count - 1; i++) {
+        values.addValue(this.low + i * step)
+      }
+    } else {
+      let step = ((this.high - average) * 2 / (this.count - 1))
+      for (let i = 1; i < this.count - 1; i++) {
+        values.addValue(this.high - i * step)
+      }
     }
-    console.log(outValue)
+    console.log(values)
   }
 }
 
 let values = new Values()
-  ;[1085, 1090, 1099, 1100].forEach(value => values.addValue(value))
+  ;[1, 2, 5, 85, 90, 95, 100].forEach(value => values.addValue(value))
 console.log(values)
 values.split()
